b9f0dca57c52af2feb8860a0df587774bfc33fb2
root <root@kali>
2023-05-23 07:44:39 +0800

    fist angular tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './views/test/test.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TestComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/routes/home/app-routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+// import { HomeComponent } from '../../views/home/home.component';
+
+import { HomeComponent } from '@views/home/home.component';
+
+export const homeRoute: Route = { path: 'home', component: HomeComponent };
